049f2f0282a0f70a41ac4fb0add58372c9ce3117
xieman <870021840@qq.com>
2019-11-27 09:12:35 +0800

    JDBC 回顾，分析存在的问题

---
+++ src/main/java/com/dgut/dao/UserDao.java
@@ -0,0 +1,38 @@
+package com.dgut.dao;
+
+import java.sql.*;
+
+public class UserDao {
+
+
+    /*
+    * 查询指定用户名
+    * */
+    public void findUsersBy(String username) throws SQLException {
+        //1. 获取数据库链接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis", "root", "xieman123");
+        //2. 创建statement
+        PreparedStatement statement = connection.prepareStatement("select * from user where username = ?");
+        statement.setString(1,username);
+        //3. 执行sql语句
+        ResultSet rs = statement.executeQuery();
+        //4. 遍历结果集
+        while (rs.next()){
+            int id = rs.getInt("id");
+            String username1 = rs.getString("username");
+            String address = rs.getString("address");
+            System.out.println(id+" "+username1+" "+address);
+        }
+        //4.关闭资源
+        rs.close();;
+        statement.close();
+        connection.close();
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        UserDao  userDao = new UserDao();
+        userDao.findUsersBy("yyy");
+    }
+
+}
---
+++ src/main/java/com/dgut/domain/User.java
@@ -0,0 +1,32 @@
+package com.dgut.domain;
+
+public class User {
+
+    Integer id;
+    String username;
+    String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
